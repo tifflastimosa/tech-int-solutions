97e1ea11bb4c77781c3c3413480378b3e0279193
tifflastimosa <lastimosa.ti@gmail.com>
2024-06-07 11:06:08 -0700

    further implement stack data structyre

--- stack/implementation/implementation/src/main/java/LinkedList/LinkedList.java
+++ stack/implementation/implementation/src/main/java/LinkedList/LinkedList.java
@@ -1,5 +1,48 @@
 package LinkedList;
 
-public class LinkedList {
+public class LinkedList<T> {
 
+  private int size;
+  private Node head;
+
+  public LinkedList() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // add : add to the front of the linked list
+  public void add(T value) {
+    // 1. create the new node
+    Node newNode = new Node(value);
+    if (this.size == 0) {
+      this.head = newNode;
+    } else {
+      newNode.setNext(this.head);
+      this.head = newNode;
+    }
+    this.size++;
+  }
+
+  // delete : remove from the front of the linked list
+  public Node delete() throws Exception {
+    if (this.size == 0) {
+      throw new Exception("Stack is empty");
+    }
+    Node temp = this.head;
+    this.head = temp.getNext();
+    temp.setNext(null);
+    this.size--;
+    return temp;
+  }
+
+  // size
+  public int getSize() {
+    return this.size;
+  }
+
+  // get the front of the linked list
+
+  public Node getHead() {
+    return this.head;
+  }
 }
--- stack/implementation/implementation/src/main/java/Stack.java
+++ stack/implementation/implementation/src/main/java/Stack.java
@@ -1,3 +1,38 @@
-public class Stack {
+import LinkedList.LinkedList;
+import LinkedList.Node;
+
+public class Stack<T> {
+
+  LinkedList stack;
+
+  public Stack() {
+    this.stack = new LinkedList();
+  }
+
+  // peek
+  public Node peek() throws Exception {
+    if (this.stack.getSize() == 0) {
+      throw new Exception("Stack is empty!");
+    }
+    return this.stack.getHead();
+  }
+
+  // pop
+  public Node pop() throws Exception {
+    if (this.stack.getSize() == 0) {
+      throw new Exception("Stack is empty!");
+    }
+    return this.stack.delete();
+  }
+
+  // push
+  public void push(T value) {
+    this.stack.add(value);
+  }
+
+  // is empty
+  public boolean isEmpty() {
+    return this.stack.getSize() == 0;
+  }
 
 }
